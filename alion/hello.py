c5b4a10b10903193ec34fe640bb6741e83dd7518
unknown <victor.gonzalez@lasallistas.org.mx>
2020-09-06 15:43:02 -0500

    Subiendo FlaksAzure

--- alion/hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for, jsonify, request
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
